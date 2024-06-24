2ce366604b1d43e2df9fd225153adb94bb4daece
ChivenZhang <1759774248@qq.com>
2024-06-24 18:01:06 +0800

    update

--- Source/Widget/Private/RmGUIWidget.cpp
+++ Source/Widget/Private/RmGUIWidget.cpp
@@ -187,68 +187,6 @@ void RmGUIWidget::handle(IRmGUIReactorRaw source, IRmGUIEventRaw event)
 
 void RmGUIWidget::layout(RmRectRaw client)
 {
-	auto layout_func = [](RmRaw<IRmGUIWidget> widget)->flex::TaitankNodeRef {
-		auto node = flex::NodeCreate();
-		flex::SetWidth(node, widget->getFixedWidth());
-		flex::SetHeight(node, widget->getFixedHeight());
-		flex::SetMinWidth(node, widget->getMinWidth());
-		flex::SetMinHeight(node, widget->getMinHeight());
-		flex::SetMaxWidth(node, widget->getMaxWidth());
-		flex::SetMaxHeight(node, widget->getMaxHeight());
-		flex::SetBorder(node, flex::CSSDirection::CSS_LEFT, widget->getBorder().X);
-		flex::SetBorder(node, flex::CSSDirection::CSS_TOP, widget->getBorder().Y);
-		flex::SetBorder(node, flex::CSSDirection::CSS_RIGHT, widget->getBorder().Z);
-		flex::SetBorder(node, flex::CSSDirection::CSS_BOTTOM, widget->getBorder().W);
-		flex::SetMargin(node, flex::CSSDirection::CSS_LEFT, widget->getMargin().X);
-		flex::SetMargin(node, flex::CSSDirection::CSS_TOP, widget->getMargin().Y);
-		flex::SetMargin(node, flex::CSSDirection::CSS_RIGHT, widget->getMargin().Z);
-		flex::SetMargin(node, flex::CSSDirection::CSS_BOTTOM, widget->getMargin().W);
-		flex::SetPadding(node, flex::CSSDirection::CSS_LEFT, widget->getPadding().X);
-		flex::SetPadding(node, flex::CSSDirection::CSS_TOP, widget->getPadding().Y);
-		flex::SetPadding(node, flex::CSSDirection::CSS_RIGHT, widget->getPadding().Z);
-		flex::SetPadding(node, flex::CSSDirection::CSS_BOTTOM, widget->getPadding().W);
-		return node;
-		};
-
-	auto root = layout_func(this);
-	flex::SetWidth(root, client->W);
-	flex::SetHeight(root, client->H);
-	flex::SetFlexDirection(root, flex::FlexDirection::FLEX_DIRECTION_ROW);
-	auto childList = getChildren();
-	for (size_t i = 0; i < childList.size(); ++i)
-	{
-		auto node = layout_func(childList[i].get());
-		if (std::isnan(childList[i]->getFixedWidth()) == false && std::isnan(childList[i]->getFixedHeight()) == false)
-		{
-			flex::SetAlignSelf(node, flex::FlexAlign::FLEX_ALIGN_START);
-		}
-		if (std::isnan(childList[i]->getFixedWidth()))
-		{
-			flex::SetFlexGrow(node, 1.0f);
-		}
-		if (std::isnan(childList[i]->getFixedHeight()))
-		{
-			flex::SetAlignSelf(node, flex::FlexAlign::FLEX_ALIGN_STRETCH);
-		}
-		root->AddChild(node);
-	}
-	flex::DoLayout(root, RmNAN, RmNAN);
-
-	if (getParent() == nullptr)
-	{
-		auto left = flex::GetLeft(root); auto top = flex::GetTop(root);
-		auto width = flex::GetWidth(root); auto height = flex::GetHeight(root);
-		setRect({ client->X + left, client->Y + top, width, height });
-	}
-	for (size_t i = 0; i < childList.size(); ++i)
-	{
-		auto node = root->GetChild(i);
-		auto left = flex::GetLeft(node); auto top = flex::GetTop(node);
-		auto width = flex::GetWidth(node); auto height = flex::GetHeight(node);
-		childList[i]->setRect({ client->X + left, client->Y + top, width, height });
-	}
-
-	flex::NodeFreeRecursive(root);
 }
 
 void RmGUIWidget::paint(IRmGUIPainterRaw painter, RmRectRaw client)
