12363a5a23dfeed1a5122c961c6aa1ccd95764ac
ChivenZhang <1759774248@qq.com>
2024-07-01 11:08:56 +0800

    update text widget

--- Source/Widget/Private/RmGUIText.cpp
+++ Source/Widget/Private/RmGUIText.cpp
@@ -1,12 +1,182 @@
 #include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+#include "RmGUIText.h"
+
+class RmGUITextPrivateData : public RmGUIWidgetPrivate
+{
+public:
+	RmGUISignalAs<int32_t /*oldPos*/, int32_t /*newPos*/> OnCursorPositionChanged;
+	RmGUISignalAs<> OnEditingFinished;
+	RmGUISignalAs<> OnInputRejected;
+	RmGUISignalAs<> OnReturnPressed;
+	RmGUISignalAs<> OnSelectionChanged;
+	RmGUISignalAs<RmString const& /*text*/> OnTextChanged;
+	RmGUISignalAs<RmString const& /*text*/> OnTextEdited;
+};
+#define PRIVATE() ((RmGUITextPrivateData*) m_PrivateText)
 
 RmGUIText::RmGUIText(IRmGUIWidgetRaw parent)
 	:
 	RmGUIControl(parent),
-	m_PrivateInput(nullptr)
+	m_PrivateText(nullptr),
+	cursorPositionChanged(nullptr),
+	editingFinished(nullptr),
+	inputRejected(nullptr),
+	returnPressed(nullptr),
+	selectionChanged(nullptr),
+	textChanged(nullptr),
+	textEdited(nullptr)
 {
+	m_PrivateText = new RmGUITextPrivateData;
+	cursorPositionChanged = &PRIVATE()->OnCursorPositionChanged;
+	editingFinished = &PRIVATE()->OnEditingFinished;
+	inputRejected = &PRIVATE()->OnInputRejected;
+	returnPressed = &PRIVATE()->OnReturnPressed;
+	selectionChanged = &PRIVATE()->OnSelectionChanged;
+	textChanged = &PRIVATE()->OnTextChanged;
+	textEdited = &PRIVATE()->OnTextEdited;
 }
 
 RmGUIText::~RmGUIText()
+{
+	delete m_PrivateText; m_PrivateText = nullptr;
+}
+
+RmGUITextStyle RmGUIText::getStyle() const
+{
+	return RmGUITextStyle();
+}
+
+void RmGUIText::setStyle(RmGUITextStyle value)
+{
+}
+
+RmGUIText::echo_t RmGUIText::getEchoMode() const
+{
+	return echo_t();
+}
+
+void RmGUIText::setEchoMode(echo_t value)
+{
+}
+
+int32_t RmGUIText::getMaxLength() const
+{
+	return 0;
+}
+
+void RmGUIText::setMaxLength(int32_t value)
+{
+}
+
+RmString RmGUIText::getPlaceholderText() const
+{
+	return RmString();
+}
+
+void RmGUIText::setPlaceholderText(RmString const& value)
+{
+}
+
+bool RmGUIText::getReadOnly() const
+{
+	return false;
+}
+
+void RmGUIText::setReadOnly(bool value)
+{
+}
+
+RmFontAligns RmGUIText::getAlignment() const
+{
+	return RmFontAligns();
+}
+
+void RmGUIText::setAlignment(RmFontAligns value)
+{
+}
+
+int32_t RmGUIText::getCursorPosition() const
+{
+	return 0;
+}
+
+int32_t RmGUIText::getCursorPosition(int32_t x, int32_t y) const
+{
+	return 0;
+}
+
+void RmGUIText::setCursorPosition(int32_t value)
+{
+}
+
+RmString RmGUIText::getSelectedText() const
+{
+	return RmString();
+}
+
+void RmGUIText::setSelectedText(RmString const value)
+{
+}
+
+void RmGUIText::getSelection(int32_t& start, int32_t& length) const
+{
+}
+
+void RmGUIText::setSelection(int32_t start, int32_t& length)
+{
+}
+
+RmString RmGUIText::getText() const
+{
+	return RmString();
+}
+
+void RmGUIText::setText(RmString const& value)
+{
+}
+
+void RmGUIText::backspace()
+{
+}
+
+void RmGUIText::del()
+{
+}
+
+void RmGUIText::deselect()
+{
+}
+
+void RmGUIText::insert(RmString const& text)
+{
+}
+
+void RmGUIText::clear()
+{
+}
+
+void RmGUIText::cut()
+{
+}
+
+void RmGUIText::redo()
+{
+}
+
+void RmGUIText::undo()
 {
 }
--- Source/Widget/Private/RmGUIText.h
+++ Source/Widget/Private/RmGUIText.h
@@ -23,23 +23,60 @@ public:
 public:
 	RmGUIText(IRmGUIWidgetRaw parent = nullptr);
 	~RmGUIText();
+
 	RmGUITextStyle getStyle() const;
 	void setStyle(RmGUITextStyle value);
-	int32_t getCursorPosition() const;
-	void setCursorPosition(int32_t value);
-	RmString getPlaceholderText() const;
-	void setPlaceholderText(RmString const& value);
+
 	echo_t getEchoMode() const;
 	void setEchoMode(echo_t value);
+
 	int32_t getMaxLength() const;
 	void setMaxLength(int32_t value);
+
+	RmString getPlaceholderText() const;
+	void setPlaceholderText(RmString const& value);
+
 	bool getReadOnly() const;
 	void setReadOnly(bool value);
+
+	RmFontAligns getAlignment() const;
+	void setAlignment(RmFontAligns value);
+
+	int32_t getCursorPosition() const;
+	int32_t getCursorPosition(int32_t x, int32_t y) const;
+	void setCursorPosition(int32_t value);
+
 	RmString getSelectedText() const;
 	void setSelectedText(RmString const value);
+
+	void getSelection(int32_t& start, int32_t& length) const;
+	void setSelection(int32_t start, int32_t& length);
+
 	RmString getText() const;
 	void setText(RmString const& value);
 
+	void backspace();
+	void del();
+	void deselect();
+	void insert(RmString const& text);
+
+	void clear();
+	void cut();
+	void redo();
+	void undo();
+
+public:
+	IRmGUISignalAsRaw<int32_t /*oldPos*/, int32_t /*newPos*/> cursorPositionChanged;
+	IRmGUISignalAsRaw<> editingFinished;
+	IRmGUISignalAsRaw<> inputRejected;
+	IRmGUISignalAsRaw<> returnPressed;
+	IRmGUISignalAsRaw<> selectionChanged;
+	IRmGUISignalAsRaw<RmString const& /*text*/> textChanged;
+	IRmGUISignalAsRaw<RmString const& /*text*/> textEdited;
+
 private:
-	RmGUIWidgetPrivateRaw m_PrivateInput;
+	RmGUIWidgetPrivateRaw m_PrivateText;
 };
+using RmGUITextRef = RmRef<RmGUIText>;
+using RmGUITextRaw = RmRaw<RmGUIText>;
+using RmGUITextEchoMode = RmGUIText::echo_t;
