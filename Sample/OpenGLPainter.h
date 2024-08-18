8c12c7bc4ec89e6583ed835a7457884a924d4333
ChivenZhang <1759774248@qq.com>
2024-08-18 10:30:33 +0800

    update

--- Sample/OpenGLPainter.h
+++ Sample/OpenGLPainter.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <OpenUI/UIPainter.h>
-#include <cairo/cairo.h>
-#include <pango/pangocairo.h>
 
 class OpenGLPainter : public UIPainter
 {
@@ -47,14 +45,6 @@ public:
 	uint32_t getTexture() const;
 	uint32_t getTextureUpdated() const;
 
-protected:
-	uint32_t m_NativeTexture;
-	cairo_t* m_NativeContext;
-	PangoLayout* m_NativeLayout;
-	cairo_surface_t* m_NativeSurface;
-	UIPen m_Pen;
-	UIFont m_Font;
-	UIBrush m_Brush;
-	UIRect m_CilpRect;
-	bool m_EnableCilp = false;
+private:
+	UIPainterPrivateRaw m_Private;
 };
