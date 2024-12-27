9e29a833ce1bf804064e5cf58b12d30d57ebb75b
ChivenZhang <1759774248@qq.com>
2024-12-28 00:33:42 +0800

    update

--- INSTALL.bat
+++
@@ -1,5 +0,0 @@
-cd %~dp0
-
-xcopy /y /q .\Output\x64-Release\vcpkg_installed\x64-windows-static-md\etc\fonts\ .\Output\x64-Release\fonts\
-
-xcopy /y /q .\Output\x64-Release\vcpkg_installed\x64-windows-static-md\etc\fonts\ .\Output\x64-Debug\fonts\
