ac484131426e445d0c2ded5fee14603cccd956de
ChivenZhang <chivenzhang@gmail.com>
2026-08-15 22:13:29 +0800

    add slang shader

--- Sample/SDLGPU/SDLGPUMerger.h
+++ Sample/SDLGPU/SDLGPUMerger.h
@@ -10,10 +10,16 @@
 *
 * =================================================*/
 #include "SDLGPURender.h"
+#include <SDL3/SDL_gpu.h>
 
 class SDLGPUMerger : public SDLGPURender
 {
 public:
     SDLGPUMerger(UICanvasRaw canvas, int width, int height);
+    ~SDLGPUMerger() override;
     void render(UIRect client, UIMat4 matrix, UIImageRaw srcImg, UIImageRaw dstImg, UIComputedStyleRaw style) override;
+
+protected:
+    SDL_GPUShader* m_VShader;
+    SDL_GPUShader* m_FShader;
 };
